0dfcb791a4b4b8d17c2f83f01a0cd66e440b08c3
Cabalag-JohnEdrick <cabalagedrick@gmail.com>
2016-07-14 22:44:13 +0800

    sorry mali yung una kong pinush

--- Tica Tac Toe 2/Tica Tac Toe 2/Form2.vb
+++ Tica Tac Toe 2/Tica Tac Toe 2/Form2.vb
@@ -1,1 +1,18 @@
-﻿
+﻿Public Class Form2
+
+    Dim turn As Integer
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        turn = 1
+        Me.BackColor = Color.LightYellow
+        Button1.BackColor = Color.LightBlue
+        Button2.BackColor = Color.LightBlue
+        Button3.BackColor = Color.LightBlue
+        Button4.BackColor = Color.LightBlue
+        Button5.BackColor = Color.LightBlue
+        Button6.BackColor = Color.LightBlue
+        Button7.BackColor = Color.LightBlue
+        Button8.BackColor = Color.LightBlue
+        Button9.BackColor = Color.LightBlue
+        Button10.BackColor = Color.LightBlue
+    End Sub
+End Class
