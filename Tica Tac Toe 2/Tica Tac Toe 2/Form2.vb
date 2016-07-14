78eab3e76ea3885b383deb237d44b846510e36d5
Cabalag-JohnEdrick <cabalagedrick@gmail.com>
2016-07-14 23:03:39 +0800

    Nilagyan ko na n g condition yung X

--- Tica Tac Toe 2/Tica Tac Toe 2/Form2.vb
+++ Tica Tac Toe 2/Tica Tac Toe 2/Form2.vb
@@ -15,4 +15,52 @@
         Button9.BackColor = Color.LightBlue
         Button10.BackColor = Color.LightBlue
     End Sub
+    Private Sub win()
+        If Button1.Text = "X" And Button2.Text = "X" And Button3.Text = "X" Then
+            MessageBox.Show("Player X wins!")
+            Label6.Text += 1
+            Call disablebuttons()
+        ElseIf Button4.Text = "X" And Button5.Text = "X" And Button6.Text = "X" Then
+            MessageBox.Show("Player X wins!")
+            Label6.Text += 1
+            Call disablebuttons()
+        ElseIf Button7.Text = "X" And Button8.Text = "X" And Button9.Text = "X" Then
+            MessageBox.Show("Player X wins!")
+            Label6.Text += 1
+            Call disablebuttons()
+        ElseIf Button1.Text = "X" And Button4.Text = "X" And Button7.Text = "X" Then
+            MessageBox.Show("Player X wins!")
+            Label6.Text += 1
+            Call disablebuttons()
+        ElseIf Button2.Text = "X" And Button5.Text = "X" And Button8.Text = "X" Then
+            MessageBox.Show("Player X wins!")
+            Label6.Text += 1
+            Call disablebuttons()
+        ElseIf Button3.Text = "X" And Button6.Text = "X" And Button9.Text = "X" Then
+            MessageBox.Show("Player X wins!")
+            Label6.Text += 1
+            Call disablebuttons()
+        ElseIf Button3.Text = "X" And Button5.Text = "X" And Button7.Text = "X" Then
+            MessageBox.Show("Player X wins!")
+            Label6.Text += 1
+            Call disablebuttons()
+
+        ElseIf Button1.Text = "X" And Button5.Text = "X" And Button9.Text = "X" Then
+            MessageBox.Show("Player X wins!")
+            Label6.Text += 1
+            Call disablebuttons()
+        End If
+    End Sub
+    Private Sub disablebuttons()
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button3.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+        Button6.Enabled = False
+        Button7.Enabled = False
+        Button8.Enabled = False
+        Button9.Enabled = False
+
+    End Sub
 End Class
